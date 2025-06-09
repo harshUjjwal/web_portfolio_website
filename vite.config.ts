605143f45c352df35e2d88ad476b86854a41afa5
harshujjwal <harshujj004@gmail.com>
2025-06-09 21:52:50 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,45 +4,38 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my_portfolio_website/",
-  server: {
-    port: 8080,
-  },
+  base: "/web_portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  publicDir: "photo",
   build: {
     outDir: "dist",
     emptyOutDir: true,
     sourcemap: false,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "vendor-react";
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react';
             }
-            return "vendor";
+            return 'vendor';
           }
         },
         assetFileNames: (assetInfo) => {
-          const fileName = assetInfo.name || "";
-          const info = fileName.split(".");
+          const fileName = assetInfo.name || '';
+          const info = fileName.split('.');
           const ext = info[info.length - 1];
           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
             return `assets/images/[name]-[hash][extname]`;
           }
           return `assets/[name]-[hash][extname]`;
         },
-        chunkFileNames: "js/[name]-[hash].js",
-        entryFileNames: "js/[name]-[hash].js",
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
       },
     },
     minify: true,
