e19f33551890c0275cabddcf4331baee68517b25
harshujjwal <harshujj004@gmail.com>
2025-06-10 00:08:06 +0530

    Fix: Update paths and routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/my_portfolio_website">
+      <BrowserRouter basename="/web_portfolio_website">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
